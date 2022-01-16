b515452957bb2bae9bab04b4abbfab6ec68419be
raelynne <atlantis05@naver.com>
2022-01-16 15:38:39 +0900

    함수 2번 완료

--- Jupyter_Python/Def.py
+++ Jupyter_Python/Def.py
@@ -34,6 +34,29 @@ def happybir(name, age):
 happybir(name, age)
 
 
+# In[12]:
+
+
+def get_plus(n1,n2):
+    return n1+n2
+
+print(get_plus(1,2))
+
+
+# In[13]:
+
+
+#소수 여부 판단하기
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2,n):  #1은 소수라서
+        if n % i == 0:
+            return False
+    return True
+is_prime(17)
+
+
 # In[ ]:
 
 
